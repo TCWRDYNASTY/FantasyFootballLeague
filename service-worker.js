72b4e8d9e6c00047f6739bbb9835099f003b0c3d
Steven <comishtcwrdynasty@protonmail.com>
2025-10-18 08:04:57 -0700

    Fix quotes and commas in service worker cache
    Corrected quotes and removed trailing commas in cached file list.

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'tcwr-main-v2'; 
+
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'bylaws.html',
+  'Flushables.html',
+  'HallofChampions.html',
+  'Records.html',
+  'contactcommish.html',
+  'dynastychat.html',
+  'main.html',
+  'rookiedraftcountdown.html',
+  'style.css',
+  'DYNASTYLOGO.jpeg',
+ 
+  'Subject.jpeg', 
+  'ainteasy.jpeg',
+  'trophy3.jpeg', 
+  'wildlifeentrance.jpeg',
+
+  'prizewheel.mp3',
+  'magic word.mp3',
+  'camp-fire.mp3',
+  'tcwr-icon-192.png',
+  'tcwr-icon-512.png'
+];
